3709b9925592efa0665144450401463078926717
vyshnav.sasidharan <sasip6028@gmail.com>
2023-01-16 23:32:53 +0530

    feat - add game stage bloc

---
+++ lib/bloc/game_stage_bloc.dart
@@ -0,0 +1,32 @@
+import 'dart:html';
+
+import 'package:flutter/foundation.dart';
+
+class GameStageBloc{
+
+  /// current state of the game
+  ValueNotifier<GameState> currentGameState = ValueNotifier<GameState>(GameState.idle);
+
+  /// current guess word
+  ValueNotifier<String> currentGuessWord = ValueNotifier<String>('');
+
+  ValueNotifier<List<BodyPart>> lostParts = ValueNotifier<List<BodyPart>>();
+}
+
+/// states of the game
+enum GameState{
+  idle,
+  running,
+  failed,
+  succeeded
+}
+
+/// body parts of hangman
+enum BodyParts{
+  head,
+  body,
+  leftLeg,
+  rightLeg,
+  leftHand,
+  rightHand
+}
