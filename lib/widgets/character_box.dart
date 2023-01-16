381bc0b7217be1db2d2ce465238ba84aabe4d8f0
vyshnav.sasidharan <sasip6028@gmail.com>
2023-01-17 00:27:33 +0530

    feat - update puzzle widget on game state change

--- lib/widgets/character_box.dart
+++ lib/widgets/character_box.dart
@@ -3,21 +3,35 @@ import 'package:flutter/material.dart';
 class CharacterBox extends StatelessWidget {
   final VoidCallback onTap;
   final bool isCurrentBox;
+  final String letter;
+  final bool isCorrectGuess;
 
   const CharacterBox(
-      {super.key, required this.onTap, this.isCurrentBox = false});
+      {super.key,
+      required this.onTap,
+      this.isCurrentBox = false,
+      required this.letter,
+      required this.isCorrectGuess});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        height: 48,
-        width: 48,
-        decoration: BoxDecoration(
-            color: isCurrentBox ? Colors.limeAccent : Colors.white,
-            borderRadius: BorderRadius.circular(4)),
-      ),
+          height: 48,
+          width: 48,
+          decoration: BoxDecoration(
+              color: isCorrectGuess ? Colors.limeAccent : Colors.white,
+              borderRadius: BorderRadius.circular(4)),
+          child: isCorrectGuess
+              ? Center(
+                  child: Text(
+                    letter,
+                    style: const TextStyle(
+                        fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                )
+              : null),
     );
   }
 }
--- lib/widgets/word_puzzle.dart
+++ lib/widgets/word_puzzle.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:hangman/bloc/game_stage_bloc.dart';
 import 'package:hangman/widgets/character_box.dart';
 
 class WordPuzzle extends StatefulWidget {
   final String guessWord;
+  final GameStageBloc gameStageBloc;
 
-  const WordPuzzle({super.key, required this.guessWord});
+  const WordPuzzle(
+      {super.key, required this.guessWord, required this.gameStageBloc});
 
   @override
   State<WordPuzzle> createState() => _WordPuzzleState();
@@ -21,19 +24,29 @@ class _WordPuzzleState extends State<WordPuzzle> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Wrap(
-        spacing: 8,
-        runSpacing: 8,
-        children: List.generate(
-            widget.guessWord.length,
-            (index) => CharacterBox(
-                  onTap: () {
-                    updateSelectedBoxIndex(index);
-                  },
-                  isCurrentBox: index == _selectedBoxIndex,
-                )),
-      ),
+    return StreamBuilder(
+      stream: widget.gameStageBloc.guessedCharacters,
+      builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
+        return (snapshot.hasData)
+            ? Container(
+                child: Wrap(
+                  spacing: 8,
+                  runSpacing: 8,
+                  children: List.generate(
+                      widget.guessWord.length,
+                      (index) => CharacterBox(
+                            onTap: () {
+                              updateSelectedBoxIndex(index);
+                            },
+                            isCurrentBox: index == _selectedBoxIndex,
+                            letter: widget.guessWord[index],
+                            isCorrectGuess: snapshot.data!
+                                .contains(widget.guessWord[index]),
+                          )),
+                ),
+              )
+            : const Center(child: CircularProgressIndicator());
+      },
     );
   }
 }
