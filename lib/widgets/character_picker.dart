1e8e7a80d46d67be4fb5d2a67a01b2c30dafbbb4
vyshnav.sasidharan <sasip6028@gmail.com>
2023-01-17 00:40:40 +0530

    feat: update character picker widget on hangman state

--- lib/widgets/character_picker.dart
+++ lib/widgets/character_picker.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:hangman/bloc/game_stage_bloc.dart';
 
 class CharacterPicker extends StatefulWidget {
-  const CharacterPicker({super.key});
+  const CharacterPicker({super.key, required this.gameStageBloc});
+  final GameStageBloc gameStageBloc;
 
   @override
   State<CharacterPicker> createState() => _CharacterPickerState();
@@ -21,32 +23,50 @@ class _CharacterPickerState extends State<CharacterPicker> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Wrap(
-        spacing: 8,
-        runSpacing: 8,
-        children: List.generate(
-            alphabets.length,
-            (index) => GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      guessedLetters.add(alphabets[index]);
-                    });
-                  },
-                  child: Container(
-                    height: 24,
-                    width: 24,
-                    decoration: BoxDecoration(
-                        color: guessedLetters.contains(alphabets[index])
-                            ? Colors.grey
-                            : Colors.white,
-                        borderRadius: BorderRadius.circular(4)),
-                    child: Center(
-                      child: Text(alphabets[index]),
-                    ),
+    return StreamBuilder(
+        stream: widget.gameStageBloc.guessedCharacters,
+        builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
+          return (snapshot.hasData)
+              ? Container(
+                  child: Wrap(
+                    spacing: 8,
+                    runSpacing: 8,
+                    children: List.generate(
+                        alphabets.length,
+                        (index) => GestureDetector(
+                              onTap: () {
+                                if (!snapshot.data!
+                                    .contains(alphabets[index])) {
+                                  guessedLetters.add(alphabets[index]);
+                                  widget.gameStageBloc
+                                      .updateGuessedCharacters(guessedLetters);
+
+                                  if (!widget
+                                      .gameStageBloc.currentGuessWord.value
+                                      .contains(alphabets[index])) {
+                                    widget.gameStageBloc.updateLostBodyParts();
+                                  }
+                                }
+                              },
+                              child: Container(
+                                height: 24,
+                                width: 24,
+                                decoration: BoxDecoration(
+                                    color: guessedLetters
+                                            .contains(alphabets[index])
+                                        ? Colors.grey
+                                        : Colors.white,
+                                    borderRadius: BorderRadius.circular(4)),
+                                child: Center(
+                                  child: Text(alphabets[index]),
+                                ),
+                              ),
+                            )),
                   ),
-                )),
-      ),
-    );
+                )
+              : const Center(
+                  child: CircularProgressIndicator(),
+                );
+        });
   }
 }
